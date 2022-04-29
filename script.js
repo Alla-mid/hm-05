1a2983701c2a96df33f60996e78f8c3e0137b45d
Alla <alla.midliar@gmail.com>
2022-04-29 11:21:43 +0200

    new

--- script.js
+++ script.js
@@ -30,16 +30,16 @@ colorElements.forEach((element, index) => {
             image.setAttribute('src', './assets/' + color + '_socks.jpeg')
         }
     });
-}
+});
 
 const rembuttonElements = document.querySelectorAll('.remFromCartBtn');
 
 for(let j = 0; j < buttonElements.length; j++) {
     rembuttonElements[j].addEventListener('click', function() {
-        const cartAmountEl1 = document.querySelector('.cartAmountEl');
-        if(cartAmountEl1) {
-            const amount1 = parseInt(cartAmountEl1.textContent, 10) || 0;
-            cartAmountEl.innerHTML = amount1 - 1
+        const cartAmountEl = document.querySelector('.cartAmount');
+        if(cartAmountEl) {
+            const amount = parseInt(cartAmountEl.textContent, 10) || 0;
+            cartAmountEl.innerHTML = amount - 1
         } else {
             console.log('element does not exist')
         }
